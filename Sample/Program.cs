5849188fae1095a50637b506d074193a14284b8d
晖明 魏 <cnwhm@hotmail.com>
2021-01-29 19:51:56 +0800

    update sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using EasyExtension.Data;
+using System;
 using System.Data;
-using System.Data.EasyData;
 namespace Sample
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            DataRow dr=null;
-            dr.ToBoolean("aaa");
+        { 
+            DataTable dt = new DataTable();
+            dt.Columns.Add("idx", typeof(int));
+            dt.Columns.Add("bolean", typeof(bool));
+            dt.Columns.Add("date", typeof(DateTime));
+            dt.Columns.Add("string", typeof(string));
+
+            DataRow dr = dt.NewRow();
+            dr["idx"] = 0;
+            dr["bolean"] = true;
+            dr["date"] = DateTime.Now;
+            dr["string"] = "asdasd";
+
+            Console.WriteLine(dr.ToBoolean("bolean"));
+            Console.WriteLine(dr.ToBoolean("idx"));
+            Console.WriteLine(dr.ToBoolean("string"));
+            Console.WriteLine(dr.ToDateTimeString("date"));
+            Console.ReadKey();
         }
     }
 }
